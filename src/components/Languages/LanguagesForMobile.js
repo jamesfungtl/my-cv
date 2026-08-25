5d9a5cea77945047563202864bc72647bb0e8dd7
gfgchhft1781 <gfgchhft1781@gmail.com>
2026-08-25 23:34:40 +0800

    amended languages section for mobile

--- src/components/Languages/LanguagesForMobile.js
+++ src/components/Languages/LanguagesForMobile.js
@@ -1,4 +1,7 @@
 import data from "../../data/data.json";
+import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import './languagesForMobile.scss';
 
 export const LanguagesForMobile = () => {
     const languageListWithId = data?.languages?.info?.map((item, idx) => ({ ...item, id: idx + 1 }));
@@ -10,11 +13,32 @@ export const LanguagesForMobile = () => {
                     {data.languages.title}
                 </h2>
                 {
-                    languageListWithId?.map(lang => (
-                        <div key={lang.id} className="item">
-                            {lang.language}
-                            <span className="lang-desc">({lang.level})</span>
-                        </div>
+                    languageListWithId?.map((lang, id) => (
+                        <Accordion
+                            key={lang.id}
+                            sx={{
+                                margin: '10px 0',
+                                borderRadius: 2,
+                                boxShadow: 1
+                            }}>
+                            <AccordionSummary
+                                expandIcon={lang?.details?.[0] && <ExpandMoreIcon />}
+                                aria-controls={`${id}-panel-content`}
+                                id={`${id}-panel-header`}
+                            >
+                                <div>
+                                    {lang.language}
+                                    <span className="lang-desc">({lang.level})</span>
+                                </div>
+                            </AccordionSummary>
+                            {lang?.details?.[0] && <AccordionDetails>
+                                <ul className="list-unstyled languages-list">
+                                    {lang.details.map((detail, idx) => (
+                                        <li key={idx}>{detail}</li>
+                                    ))}
+                                </ul>
+                            </AccordionDetails>}
+                        </Accordion>
                     ))
                 }
             </div>}
